80785a52868c0d2403530a21360a8ffccb9adc3a
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-04-12 13:01:50 +0000

    import skeakers

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,3 +1,17 @@
+import data from "../../data.json";
+
 export default function Speakers() {
-  return <div>Speakers page</div>;
+  const speakersData = data.filter((item) => item.category === "speakers");
+  return (
+    <>
+      {speakersData.map((speaker) => (
+        <div key={speaker.id}>
+          <h1>{speaker.name}</h1>
+          <p>{speaker.description}</p>
+          <p>{speaker.features}</p>
+          <img src={speaker.image.desktop} />
+        </div>
+      ))}
+    </>
+  );
 }
