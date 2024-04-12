06dc18e5e8489abe32b5c8a6749bfff04fcba5c0
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-04-12 12:48:09 +0000

    import heaphones

--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -1,8 +1,18 @@
+import data from "../../data.json";
+
 export default function Headphones() {
+  const headphonesData = data.filter((item) => item.category === "headphones");
+
   return (
     <>
-      <div>Headphones page</div>
-      <p>Headphones</p>
+      {headphonesData.map((headphone) => (
+        <div key={headphone.id}>
+          <h1>{headphone.name}</h1>
+          <p>{headphone.description}</p>
+          <p>{headphone.features}</p>
+          <img src={headphone.image.desktop} />
+        </div>
+      ))}
     </>
   );
 }
