1b3b44609c1fd3c81e3714b32f4aad224796bc47
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-04-12 13:42:07 +0000

    update earphone css

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -1,18 +1,38 @@
 import data from "../../data.json";
+import styled from "styled-components";
 
 export default function Earphones() {
   const earphonesData = data.filter((item) => item.category === "earphones");
 
   return (
-    <>
+    <Wrapper>
       {earphonesData.map((earphone) => (
         <div key={earphone.id}>
           <h1>{earphone.name}</h1>
-          <p>{earphone.description}</p>
-          <p>{earphone.features}</p>
-          <img src={earphone.image.desktop} />
+          <div className="main">
+            <img src={earphone.image.desktop} />
+            <div>
+              <p>{earphone.description}</p>
+              <p>{earphone.features}</p>
+              <p>$ {earphone.price}</p>
+            </div>
+          </div>
         </div>
       ))}
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 2em;
+  img {
+    width: 40%;
+  }
+
+  .main {
+    display: flex;
+    align-items: center;
+    text-align: justify;
+    gap: 2rem;
+  }
+`;
