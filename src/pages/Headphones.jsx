f7b519bcba79532b2f5236dfddbed883202d1b52
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-04-17 11:14:09 +0000

    update headphones css

--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -1,18 +1,38 @@
 import data from "../../data.json";
+import styled from "styled-components";
 
 export default function Headphones() {
   const headphonesData = data.filter((item) => item.category === "headphones");
 
   return (
-    <>
+    <Wrapper>
       {headphonesData.map((headphone) => (
         <div key={headphone.id}>
           <h1>{headphone.name}</h1>
-          <p>{headphone.description}</p>
-          <p>{headphone.features}</p>
-          <img src={headphone.image.desktop} />
+          <div className="main">
+            <img src={headphone.image.desktop} />
+            <div>
+              <p>{headphone.description}</p>
+              <p>{headphone.features}</p>
+              <p>$ {headphone.price}</p>
+            </div>
+          </div>
         </div>
       ))}
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 2em;
+  img {
+    width: 40%;
+  }
+
+  .main {
+    display: flex;
+    align-items: center;
+    text-align: justify;
+    gap: 2rem;
+  }
+`;
